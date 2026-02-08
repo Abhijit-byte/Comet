9b030529ccffc10355f2281128ab0037af04171b
Abhijit Dash <atd5370@gmail.com>
2026-02-08 11:05:23 +0530

    Update settings.py

--- django_project/cosmic_watch/settings.py
+++ django_project/cosmic_watch/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cosmic-watch-dev-key'
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,comet-production-6342.up.railway.app').split(',')
 
 INSTALLED_APPS = [
     'daphne',
